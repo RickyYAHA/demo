e888792531beda711a0d2588c3fa8f80488481d2
RickyYAHA <kasper.skuratowicz159@wp.pl>
2022-11-16 11:57:34 +0100

    s

--- .vscode/class/index.php
+++ .vscode/class/index.php
@@ -4,6 +4,9 @@ echo "<pre>";
 $db = new mysqli('localhost','root', '', 'loginform' );
 $user = new User("jkowalski", "tajneHasło");
 $user->login();
-
-
+if($user->isAuth()) {
+    echo "użytkownik zalogowany poprawnie";
+} else {
+    echo "Błąd logowania";
+}
 ?>
