ddf67daeffda9746c1df2c6fe793339bd2101c48
RickyYAHA <kasper.skuratowicz159@wp.pl>
2022-11-16 11:27:42 +0100

    nig

--- .vscode/class/User.class.php
+++ .vscode/class/User.class.php
@@ -10,13 +10,27 @@ class User {
     public function __construct(string $login, string $password)
     {
                 $this->login = $login;
-                $this->password_hash = password_hash($password, PASSWORD_ARGON21);
+                $this->password_hash = $password;
+            global $db;
+            $this->db = &$db;
     }
     public function isAuth() : bool {
-
+        if(isset($this->id) && $this->id != null)
+            return true;
+        else
+            return false;
     }
     public function login(){
-        global $db;
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query);
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        $row = $result->fetch_assoc();
+        if(password_verify($this->password, $row['password'])) {
+            $this->id = $row['id'];
+            $this->firstName = $row['firstName'];
+            $this->lastName = $row['lastName'];
     }
     public function logout(){
 
