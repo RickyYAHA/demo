ae0324d65e9a923e22084185894b0e755d18130d
RickyYAHA <kasper.skuratowicz159@wp.pl>
2022-11-23 11:15:14 +0100

    we

--- .vscode/class/index.php
+++ .vscode/class/index.php
@@ -1,12 +1,20 @@
 <?php
-require('User.class.php');
-echo "<pre>";
-$db = new mysqli('localhost','root', '', 'loginform' );
-$user = new user("Kasper", "haslo");
-$user->login();
-if($user->isAuth()) {
-    echo "użytkownik zalogowany poprawnie";
+require_once('conf.php');
+require_once('User.class.php');
+
+$user = new User('Kasper', 'haslo123');
+
+if($user->register()) {
+    echo "Zarejestrowano poprawnie";
 } else {
-    echo "Błąd logowania";
+    echo "Błąd rejestracji użytkownika";
 }
+
+
+if($user->login()) {
+    echo "Zalogowano!";
+} else {
+    echo "Zły login bądź hasło";
+}
+
 ?>
