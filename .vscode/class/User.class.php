f49ef87ac2f122342143edc77df839ea9e9e2e43
RickyYAHA <kasper.skuratowicz159@wp.pl>
2022-11-23 11:47:08 +0100

    yth

--- .vscode/class/User.class.php
+++ .vscode/class/User.class.php
@@ -1,58 +1,73 @@
 <?php
-class user {
-    private int $id;
+class User {
+    private $db;
+    private int $info;
     private string $login;
     private string $password;
-    private string $firstname;
-    private string $lastname;
+    private string $firstName;
+    private string $lastName;
+
 
     public function __construct(string $login, string $password)
     {
-                $this->login = $login;
-                $this->password = $password;
-            global $db;
-            $this->db = &$db;
+        $this->login = $login;
+        $this->password = $password;
+        global $db;
+        $this->db = &$db;
     }
     public function isAuth() : bool {
+        
         if(isset($this->id) && $this->id != null)
             return true;
         else
             return false;
     }
-    public function login(){
-             $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+    public function login() {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
         $preparedQuery = $this->db->prepare($query);
         $preparedQuery->bind_param('s', $this->login);
         $preparedQuery->execute();
         $result = $preparedQuery->get_result();
-        $row = $result->fetch_assoc();
-        $passwordHash = $row['password'];
-        if(password_verify($this->password, $passwordHash)) {
-            $this->id = $row['Info'];
-            $this->firstName = $row['firstName'];
-            $this->lastName = $row['lastName'];
-        
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            if(password_verify($this->password, $row['password'])) {
+                $this->info = $row['info'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+            }
         }
-
-}
-    public function logout(){
-
+        
     }
-    public function register(){
+    public function logout() {
+        $this->login = null;
+        $this->password = null;
+        $this->id = null;
+        $this->firstName = null;
+        $this->lastName = null;
+    }
+    public function register() {
         $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
         $preparedQuery = $this->db->prepare($query);
-        $password = password_Hash($this->password);
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
         if(!isset($this->firstName))
-            $this->firstName = "Kasper";
+            $this->firstName = "";
         if(!isset($this->lastName))
-            $this->lastName = "Skuratowicz";
-        $preparedQuery->bind_param('Kasper', $this->login, 
-                                            $password,
+            $this->lastName = "";
+        $preparedQuery->bind_param('ssss', $this->login, 
+                                            $passwordHash,
                                             $this->firstName, 
                                             $this->lastName);
         $preparedQuery->execute();
     }
-
+    public function setFirstName(string $firstName) {
+        $this->firstName = $firstName;
+    }
+    public function setLastName(string $lastName) {
+        $this->lastName = $lastName;
+    }
+    public function getName() : string {
+        return $this->firstName . " " . $this->lastName;
+    }
 }
 
 
